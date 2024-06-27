c3427a6ca47b8cdc3fe42fc3fea6823ac0095e33
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2024-06-27 09:36:31 -0300

    feat: backup project

---
+++ SistemaDeTarefas/Enums/TaskStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum TaskStatus
+    {
+        [Description("ToDo")]
+        ToDo = 1,
+        [Description("Pending")]
+        Pending = 2,
+        [Description("Finished")]
+        Finished = 3,
+    }
+}
